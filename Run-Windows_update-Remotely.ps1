320a5ebe60a067f804142935de1298fd1b1785ef
Samsoft <52179120+flaschracket@users.noreply.github.com>
2023-06-13 11:01:13 +0200

    Update Run-Windows_update-Remotely.ps1
    adding commands that were required to run in Target to make a permanent session

--- Run-Windows_update-Remotely.ps1
+++ Run-Windows_update-Remotely.ps1
@@ -1,4 +1,13 @@
-
+#thanks to https://stackoverflow.com/questions/7078958/powershell-remote-microsoft-update-session-access-denied-0x80070005
+#Please do these commands in Target host before running the script 
+#--------------------
+#New-PSSessionConfigurationFile -RunAsVirtualAccount -Path .\VirtualAccount.pssc
+## Note this will restart the WinRM service:
+#Register-PSSessionConfiguration -Name 'VirtualAccount'  -Path .\VirtualAccount.pssc -Force
+## Check the Permission property:
+#Get-PSSessionConfiguration -Name 'VirtualAccount'
+## Those users will have full unrestricted access to the system!
+#--------------------
 $computerName = "IP/Name"
 $username = "admin user"
 $password = ConvertTo-SecureString -String "password" -AsPlainText -Force
@@ -7,6 +16,7 @@ $session = @()
 
 $script = {
             write-host "Hello from remote target"
+            #filepath is in Remote/target host
             $job = Start-job -FilePath c:\Windows_Update_2.ps1 -Verbose -Credential $cred
             echo $job
             Wait-Job $job
